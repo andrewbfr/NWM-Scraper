7e55a8c6f9900445597c819560a82a3ae6563c26
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2018-02-06 23:13:32 -0700

    burnt toast, flailing

--- server.js
+++ server.js
@@ -38,7 +38,51 @@ require("./routes/api-routes.js")(app);
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/thisController.js");
+var router = express.Router();
 
+// Require all models
+var db = require("./models");
+// Routes
+console.log("I am html-routes");
+
+router.get("/", function(req, res) {
+
+router.get("/list", function(req, res) {
+    console.log("searching for database");
+    db.Article
+    .find({})
+    .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+    });
+    console.log("searched DB, rendering article objects");
+    res.render("list", dbArticle);
+});
+});
+// loads the list page and awaits scrape
+router.get("/list", function(req, res) {
+console.log("searching for database");
+console.log(`This is the "res": ${res}`);
+    db.Article
+    .find({})
+    .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        // res.json(dbArticle);
+        console.log(dbArticle[0]);
+        console.log("searched DB, rendering article objects");
+        return res.render("list", dbArticle);
+    })
+    .catch(function(err) {
+        // If an error occurred, send it to the client
+        return res.json(err);
+    }); 
+    // res.end();
+    
+});
 app.use(routes);
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
